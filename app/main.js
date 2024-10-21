df246beeacb61de9c7ff8e74ef66cb25655e6240
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 10:12:36 +0530

    stage 1 200 ok

--- app/main.js
+++ app/main.js
@@ -2,9 +2,12 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    socket.on('data',()=>{
+        socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    })
+    socket.on("close", () => {
     socket.end();
-  });
+    });
 });
 
 server.listen(4221, "localhost");
