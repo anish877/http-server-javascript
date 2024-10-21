2bb52b531098caec01efcc3c072dfa02c44463fa
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 11:14:54 +0530

    stage 3 respond with body

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const server = net.createServer((socket) => {
         const subData = data.toString().split(' ')
         if(subData[1].charAt(0)==='/'){
             const text = subData[1].split('/')
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text[2].length}\r\n\r\n${text[2]}`)
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text[text.length-1].length}\r\n\r\n${text[text.length-1]}`)
         }
         else{
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
