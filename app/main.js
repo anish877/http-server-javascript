483c61d99545212f5992518b14c273a27f5d2bca
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-22 00:08:28 +0530

    stage 10 gzip compression

--- app/main.js
+++ app/main.js
@@ -62,7 +62,13 @@ const server = net.createServer((socket) => {
             }
             else if(subData[1].split('/').length==3){
                 const text = subData[1].split('/')
-                const compressed = zlib.gzipSync(text[text.length-1])
+                let compressed
+                if(contentEncoding==''){
+                    compressed = text[text.length-1]
+                }
+                else{
+                    compressed = zlib.gzipSync(text[text.length-1])
+                }
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n${contentEncoding}Content-Length: ${compressed.toString().length}\r\n\r\n`)
                 socket.write(compressed)
             }
