040e9f3be515ba02284767c17e9b8d629281cb7f
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 21:35:27 +0530

    stage 10 gzip compression

--- app/main.js
+++ app/main.js
@@ -70,9 +70,11 @@ const server = net.createServer((socket) => {
                 }
             }
             else if(subData[1].split('/').length==3){
+
                 const text = subData[1].split('/')
                 const compressedData = compress(text[text.length-1],gzip)
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n${contentEncoding}Content-Length: ${text[text.length-1].length}\r\n\r\n${compressedData}`)
+                
             }
             else{
                 socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
