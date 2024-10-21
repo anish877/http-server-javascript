6e7e604e52897c50e0128cebe22499523db11b19
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 20:28:52 +0530

    stage 8 compression headers

--- app/main.js
+++ app/main.js
@@ -23,7 +23,7 @@ const server = net.createServer((socket) => {
         const subData = data.toString().split(' ')
         const [acceptEncoding,compressionScheme] = data.toString().split('\n')[2].split(' ')
         console.log(acceptEncoding+compressionScheme)
-        if(acceptEncoding==='Accept-Encoding:' && compressionScheme==='gzip'){
+        if(acceptEncoding==="Accept-Encoding:" && compressionScheme==="gzip"){
             contentEncoding = 'Content-Encoding: gzip\r\n'
             console.log(contentEncoding)
         }
