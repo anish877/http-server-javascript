90b0ed152b48bf4e399d8495d81aae7488f9c5aa
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 11:07:27 +0530

    stage 3 respond with body

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on('data',(data)=>{
         const subData = data.toString().split(' ')
-        if(subData[1]==='/'){
+        if(subData[1].charAt(0)==='/'){
             const text = subData[1].split('/')
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text[1].length}\r\n\r\ `+text[1])
         }
