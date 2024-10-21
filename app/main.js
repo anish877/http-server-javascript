8aebf86cf0ed9fdff1e2126b7af1602e54df23c9
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 13:20:04 +0530

    stage 6 return a file

--- app/main.js
+++ app/main.js
@@ -2,9 +2,9 @@ const net = require("net");
 const fs = require('fs');
 const path = require("path");
 
-const command = process.argv[1]
+const command = process.argv[2]
 console.log(command)
-const option = process.argv[2]
+const option = process.argv[3]
 let filePath
 
 switch (command) {
