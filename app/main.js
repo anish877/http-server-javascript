1c6f41f8dc835cdf24a2b47c0999358ddfd824e5
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-21 12:00:45 +0530

    stage 4 read header

--- app/main.js
+++ app/main.js
@@ -10,6 +10,7 @@ const server = net.createServer((socket) => {
         else if(subData[1]==='/user-agent'){
             const userAgentLine = data.toString().split('\r\n')[2]
             console.log(userAgentLine)
+            const userAgent = userAgentLine.slice(userAgentLine.indexOf(" ")+1)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
         }
         else if(subData[1].split('/').length==3){
